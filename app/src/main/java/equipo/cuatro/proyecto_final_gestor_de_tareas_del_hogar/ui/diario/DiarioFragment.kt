818da83815dba2549c41f23c392791c995619140
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2025-03-05 10:38:29 -0700

    Pantalla de Unirse

--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioFragment.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.databinding.FragmentDiarioBinding
