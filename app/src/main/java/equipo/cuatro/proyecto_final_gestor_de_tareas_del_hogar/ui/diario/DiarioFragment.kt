cafaf57e4b062ca8bb1ccf536b2fbaed3201ed23
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2025-03-06 10:00:15 -0700

    Fragmentos fix

--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioFragment.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioFragment.kt
@@ -1,4 +1,4 @@
-package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.ui.dashboard
+package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.ui.diario
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.databinding.FragmentDashboardBinding
+import equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.databinding.FragmentHomeBinding
 
-class DashboardFragment : Fragment() {
+class DiarioFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val homeViewModel =
+            ViewModelProvider(this).get(DiarioViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.texthome
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioViewModel.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/ui/diario/DiarioViewModel.kt
@@ -1,10 +1,10 @@
-package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.ui.home
+package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.ui.diario
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class DiarioViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
