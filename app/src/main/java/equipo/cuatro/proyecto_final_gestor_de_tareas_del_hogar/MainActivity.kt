4b6fb0735c76def31811180f5adbf6f883545992
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2025-03-18 21:03:16 -0700

    Lógica de Login

--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/MainActivity.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/MainActivity.kt
@@ -3,13 +3,27 @@ package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email: EditText = findViewById(R.id.editTextUsuario)
+        val password: EditText = findViewById(R.id.editTextContrasena)
 
         // Botón para ir a la pantalla de Registrarse
         val buttonRegistrarse = findViewById<Button>(R.id.buttonRegistrarse)
@@ -20,10 +34,37 @@ class MainActivity : AppCompatActivity() {
 
         val buttonIniciarSesion = findViewById<Button>(R.id.buttonIniciarSesion)
         buttonIniciarSesion.setOnClickListener {
-            val intent = Intent(this@MainActivity, HogarActivity::class.java)
-            startActivity(intent)
+            login(email.text.toString(), password.text.toString())
         }
 
+    }
+
+    fun goToMain(user: FirebaseUser) {
+        val intent = Intent(this, HogarActivity::class.java)
+        intent.putExtra("user", user.email)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            goToMain(currentUser)
+        }
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                goToMain(user!!)
+            } else {
+                Toast.makeText(baseContext, "Email y/o contraseña incorrectos", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
 }
--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/RegistrarseActivity.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/RegistrarseActivity.kt
@@ -2,22 +2,57 @@ package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class RegistrarseActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registrarse)
 
+        auth = FirebaseAuth.getInstance()
+
+        val user: EditText = findViewById(R.id.editTextUsuario)
+        val mail: EditText = findViewById(R.id.editTextMail)
+        val password: EditText = findViewById(R.id.editTextContrasenia)
+        val confirmPassword: EditText = findViewById(R.id.editTextConfirmarContrasena)
+
         val registar: Button = findViewById(R.id.buttonRegistrarse)
 
         registar.setOnClickListener {
-            val intent: Intent = Intent(this, HogarActivity::class.java)
-            startActivity(intent)
+            if (user.text.isEmpty() || mail.text.isEmpty() || password.text.isEmpty() || confirmPassword.text.isEmpty()) {
+                Toast.makeText(baseContext, "Todos los campos deben llenarse", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (!password.text.toString().equals(confirmPassword.text.toString())) {
+                Toast.makeText(baseContext, "Las contraseñas no coinciden", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                signIn(mail.text.toString(), password.text.toString())
+            }
         }
     }
+
+    fun signIn(email: String, password: String) {
+        Log.d("INFO REGISTRO", "email: ${email}, password: ${password}")
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d("INFO REGISTRO", "signInWithEmail:success")
+                val user = auth.currentUser
+                val intent = Intent(this, HogarActivity::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            } else {
+                Log.w("ERROR REGISTRO", "signInWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "El registro falló", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
