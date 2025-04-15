63a4f86982f46b8f4588d6dd60e44204c5660634
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2025-04-15 11:30:20 -0700

    El nombre de la casa en las TareasActivity ya no es hardcodeado

--- app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/TareasActivity.kt
+++ app/src/main/java/equipo/cuatro/proyecto_final_gestor_de_tareas_del_hogar/TareasActivity.kt
@@ -2,22 +2,29 @@ package equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.databinding.ActivityTareasBinding
+import equipo.cuatro.proyecto_final_gestor_de_tareas_del_hogar.domain.Home
 
 class TareasActivity : AppCompatActivity() {
-
+    private lateinit var homeRef: DatabaseReference
     private lateinit var binding: ActivityTareasBinding
+    private lateinit var hogarId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        homeRef = FirebaseDatabase.getInstance().getReference("homes")
 
         binding = ActivityTareasBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -36,11 +43,38 @@ class TareasActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        val bundle = intent.extras
+
+        if (bundle != null) {
+            hogarId = bundle.getString("HOGAR_ID").toString()
+        }
+
+        getHomeById(hogarId) { home ->
+            if (home != null) {
+                findViewById<TextView>(R.id.texthome).text = home.name
+            } else {
+                Toast.makeText(this, "Hogar no encontrado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         val agregarTarea: FloatingActionButton = findViewById(R.id.fab)
 
         agregarTarea.setOnClickListener {
             val intent = Intent(this, AgregarTareaActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun getHomeById(homeId: String, callback: (Home?) -> Unit) {
+        homeRef.child(homeId).get().addOnSuccessListener { snapshot ->
+            if (snapshot.exists()) {
+                val home = snapshot.getValue(Home::class.java)
+                callback(home)
+            } else {
+                callback(null)
+            }
+        }.addOnFailureListener {
+            callback(null)
         }
     }
 }
